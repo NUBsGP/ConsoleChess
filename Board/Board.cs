eb8e2a5f15ce55612041b68fcdfa13afe1d266bf
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-04-16 10:55:06 -0300

    adding the board

---
+++ Board/Board.cs
@@ -0,0 +1,18 @@
+﻿
+namespace XadrezInConsole.Board
+{
+    
+    class Board
+    {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+        public Piece[,] Piece { get; set; }
+
+        public Board(int lines, int columns)
+        {
+            Lines = lines;
+            Columns = columns;
+            Piece = new Piece[Lines, Columns];
+        }
+    }
+}
