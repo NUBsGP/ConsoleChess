bf338b809c494fb186c780131ac072db31cb08d3
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-04-16 10:33:19 -0300

    first class created

---
+++ Board/Position.cs
@@ -0,0 +1,18 @@
+﻿namespace Board
+{
+    class Position
+    {
+        public int X {  get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return $"{X}, {Y}";
+        }
+    }
+}
